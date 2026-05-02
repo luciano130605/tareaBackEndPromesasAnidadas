3626085755a233d89bafc024fe71f498266f4f4e
luciano <luciano.delapena@gmail.com>
2026-05-02 15:01:19 -0300

    ejercicio 3 completo

--- ejercicio1/miIndex.js
+++ ejercicio1/miIndex.js
@@ -1,6 +1,47 @@
-const color = require('colors')
+const cuenta = require('../ejercicio3/cuentaBancaria')
+const ins = require('../ejercicio3/instituto')
+const venta = require("../ejercicio3/mercadoRegistrado")
+
+console.log("ejercicio 3:") +
+
+    console.log("Cuenta bancaria")
+
+cuenta.validarSaldo(400)
+    .then((saldo) => {
+        console.log("Transferencia realizada".green)
+        return cuenta.transferir(150)
+    })
+    .catch((error) => {
+        return console.log(("Error: " + error).red)
+    })
+
+console.log("Instituto")
+
+ins.inscripcion(ins.alumno, "Base de datos")
+    .then(() => {
+        console.log("Inscripto".green);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
 
-console.log("error".red)
-console.log("éxito".green)
-console.log("info".yellow)
+console.log("Ventas")
+
+venta.validarStock("Mouse", venta.productos)
+    .then(producto => {
+        console.log("Stock existente".green)
+        return venta.realizarVenta(producto, 2)
+    })
+    .then(producto => {
+        console.log("Venta realizada".green)
+        return venta.imprimirEtiqueta(producto)
+    })
+    .then(() => {
+        console.log("Etiqueta generada".yellow)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
