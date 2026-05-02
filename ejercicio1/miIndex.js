39cd3a2c78c4cb9d5672a35185c6d83345163b5e
luciano <luciano.delapena@gmail.com>
2026-05-02 15:00:21 -0300

    ejercicio 3 completo en miIndex

--- ejercicio1/miIndex.js
+++ ejercicio1/miIndex.js
@@ -2,28 +2,28 @@ const cuenta = require('../ejercicio3/cuentaBancaria')
 const ins = require('../ejercicio3/instituto')
 const venta = require("../ejercicio3/mercadoRegistrado")
 
-// console.log("ejercicio 3:") +
+console.log("ejercicio 3:") +
 
-//     console.log("Cuenta bancaria")
+    console.log("Cuenta bancaria")
 
-// cuenta.validarSaldo(400)
-//     .then((saldo) => {
-//         console.log("Transferencia realizada".green)
-//         return cuenta.transferir(150)
-//     })
-//     .catch((error) => {
-//         return console.log(("Error: " + error).red)
-//     })
+cuenta.validarSaldo(400)
+    .then((saldo) => {
+        console.log("Transferencia realizada".green)
+        return cuenta.transferir(150)
+    })
+    .catch((error) => {
+        return console.log(("Error: " + error).red)
+    })
 
-// console.log("Instituto")
+console.log("Instituto")
 
-// ins.inscripcion(ins.alumno, "Base de datos")
-//     .then(() => {
-//         console.log("Inscripto".green);
-//     })
-//     .catch((error) => {
-//         console.log(error);
-//     });
+ins.inscripcion(ins.alumno, "Base de datos")
+    .then(() => {
+        console.log("Inscripto".green);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
 
 console.log("Ventas")
