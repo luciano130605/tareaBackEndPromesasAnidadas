4d55936494a80b44ebc9f5561c2fa300360d82a9
luciano <luciano.delapena@gmail.com>
2026-05-04 19:58:23 -0300

    miIndex.js ejercicio 4

--- ejercicio1/miIndex.js
+++ ejercicio1/miIndex.js
@@ -1,47 +1,11 @@
-const cuenta = require('../ejercicio3/cuentaBancaria')
-const ins = require('../ejercicio3/instituto')
-const venta = require("../ejercicio3/mercadoRegistrado")
+const {
+    ejecutarCuenta,
+    ejecutarInstituto,
+    ejecutarVentas
+} = require('../ejercicio4/ejecucion')
 
-console.log("ejercicio 3:") +
+console.log("Ejercicio 3")
 
-    console.log("Cuenta bancaria")
-
-cuenta.validarSaldo(400)
-    .then((saldo) => {
-        console.log("Transferencia realizada".green)
-        return cuenta.transferir(150)
-    })
-    .catch((error) => {
-        return console.log(("Error: " + error).red)
-    })
-
-console.log("Instituto")
-
-ins.inscripcion(ins.alumno, "Base de datos")
-    .then(() => {
-        console.log("Inscripto".green);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-
-
-console.log("Ventas")
-
-venta.validarStock("Mouse", venta.productos)
-    .then(producto => {
-        console.log("Stock existente".green)
-        return venta.realizarVenta(producto, 2)
-    })
-    .then(producto => {
-        console.log("Venta realizada".green)
-        return venta.imprimirEtiqueta(producto)
-    })
-    .then(() => {
-        console.log("Etiqueta generada".yellow)
-    })
-    .catch((error) => {
-        console.log(error)
-    })
-
-
+ejecutarCuenta()
+ejecutarInstituto()
+ejecutarVentas()
